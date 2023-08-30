5817f3d07b56726b1a110c7820f1e161d7d20150
Vahid Farid <vahidfarid@gmail.com>
2023-08-30 11:48:24 +0330

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,6 +2,26 @@ export interface RemoteSocketWrapper {
     value: Socket | null;
 }
 
+export interface CustomArrayBuffer {
+    earlyData: ArrayBufferLike | null,
+    error: any
+}
+
+export interface VlessHeader {
+    hasError: boolean,
+    message: string | undefined,
+    addressRemote: string,
+    addressType: number,
+    portRemote: number,
+    rawDataIndex: number,
+    vlessVersion: Uint8Array,
+    isUDP: boolean,
+}
+
+export interface UDPOutbound {
+    write: CallableFunction
+}
+
 export interface Env {
     settings: KVNamespace
 }
