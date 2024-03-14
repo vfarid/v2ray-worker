f4e8a17c5a35ac9e4dea69c19cf338745192186f
Vahid Farid <farid@aradsolution.com>
2024-03-14 16:27:26 +0330

    Update panel.ts

--- src/panel.ts
+++ src/panel.ts
@@ -82,7 +82,7 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
           <button id="btn-cn" class="btn btn-lang btn-outline-primary btn-sm rounded-2" type="button">CN</button>
         </div>
         <div class="p-2 border-bottom border-primary border-2">
-          <div class="text-nowrap fs-4 fw-bold text-dark">
+          <div class="text-nowrap fs-5 fw-bold text-dark">
             V2RAY Worker - Control Panel &nbsp;&nbsp;<span class="text-nowrap fs-6 text-info">Version ${version}</span>
           </div>
         </div>
@@ -217,7 +217,7 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
           <button type="submit" name="save" value="save" class="btn btn-primary">Save / ذخیره</button>
           <button type="submit" name="reset" value="reset" class="btn btn-warning">Reset / بازنشانی</button>
         </form>
-        <div class="p-1 border-top border-3 border-primary">
+        <div class="p-1 border-top border-2 border-primary">
           <div class="text-nowrap fs-6">
             Copyright 2024 Vahid Farid<br/>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="octicon">
@@ -237,8 +237,8 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
             </svg>
             <a class="link-dark link-offset-2" href="https://github.com/vfarid" target="_blank">vfarid</a>            </p>
           </div>
+        </div>
       </div>
-  </div>
     </body>
     <script>
       window.addEventListener('message', function (event) {
@@ -260,12 +260,15 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
       </head>
       <body dir="ltr">
-        <div class="container border p-0">
-          <div class="p-1 bg-primary text-white">
-            <div class="text-nowrap fs-4 fw-bold text-center">V2RAY Worker - Control Panel</div>
-            <div class="text-nowrap fs-6 text-center">
-              Version ${version} by
-              <a href="https://twitter.com/vahidfarid" target="_blank" class="text-white">Vahid Farid</a>
+        <div class="container border mt-3 p-0 border-primary border-2 rounded">
+          <div class="btn-group float-end m-2" role="group" dir="ltr">
+            <button id="btn-fa" class="btn btn-lang btn-outline-primary btn-sm rounded-2" type="button">FA</button>&nbsp;
+            <button id="btn-en" class="btn btn-lang btn-outline-primary btn-sm rounded-2" type="button">EN</button>&nbsp;
+            <button id="btn-cn" class="btn btn-lang btn-outline-primary btn-sm rounded-2" type="button">CN</button>
+          </div>
+          <div class="p-2 border-bottom border-primary border-2">
+            <div class="text-nowrap fs-5 fw-bold text-dark">
+              V2RAY Worker - Control Panel &nbsp;&nbsp;<span class="text-nowrap fs-6 text-info">Version ${version}</span>
             </div>
           </div>
           <div class="px-5 py-2 bg-light">
@@ -291,6 +294,27 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
           <div class="mx-5 my-2 p-1 border bg-success text-white text-center">
             <p>You can continue using your worker without control panel.</p>  
             <p>شما می‌توانید از ورکر خود بدون کنترل پنل استفاده نمایید.</p>  
+          </div>
+          <div class="p-1 border-top border-2 border-primary">
+            <div class="text-nowrap fs-6">
+              Copyright 2024 Vahid Farid<br/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" class="octicon">
+                <g clip-path="url(#clip0_1668_3024)">
+                  <path d="M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z" fill="currentColor"></path>
+                </g>
+                <defs>
+                  <clipPath id="clip0_1668_3024">
+                    <rect width="16" height="16" fill="white"></rect>
+                  </clipPath>
+                </defs>
+              </svg>
+              <a class="link-dark link-offset-2" href="https://twitter.com/vahidfarid" target="_blank">@vahidfarid</a><br/>
+              
+              <svg height="16" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-mark-github v-align-middle color-fg-default">
+                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+              </svg>
+              <a class="link-dark link-offset-2" href="https://github.com/vfarid" target="_blank">vfarid</a>            </p>
+            </div>
           </div>
         </div>
       </body>
