35fdd4bc1f55baa8bb8667cdf88ddaf12aed37ed
Vahid Farid <farid@aradsolution.com>
2024-03-27 16:21:27 +0330

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -64,7 +64,7 @@ export function EncodeConfig(conf: Config): string {
       }?encryption=${
         encodeURIComponent(conf.encryption || "none")
       }&type=${
-        conf.type
+        conf.network
       }${
         conf.path ? "&path=" + encodeURIComponent(conf.path) : ""
       }${
