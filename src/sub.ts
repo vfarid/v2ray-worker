4efc6ef59426992ed1debe3a86b9e26c1cf91019
Vahid Farid <farid@aradsolution.com>
2024-03-27 12:00:06 +0330

    Update sub.ts

--- src/sub.ts
+++ src/sub.ts
@@ -2,6 +2,10 @@ import { Buffer } from 'buffer'
 import { EncodeConfig } from './config'
 import { Config } from './interfaces'
 
+export function ToRawSubscription(configList: Array<Config>): string {
+  return configList.map(EncodeConfig).join("\n")
+}
+
 export function ToBase64Subscription(configList: Array<Config>): string {
   return Buffer.from(configList.map(EncodeConfig).join("\n"), "utf-8").toString("base64")
 }
